9ff86aa706689cd47cad46836b97679ddeb79552
JuanIgnacioMarchetto <tu@email.com>
2024-01-03 10:14:38 -0300

    3/1/2023-09:56

--- src/App.jsx
+++ src/App.jsx
@@ -17,21 +17,12 @@ const FollowMouse = () => {
       window.addEventListener('pointermove', handleMove)
     }
 
-    // cleanup:
-    // -> cuando el componente se desmonta
-    // -> cuando cambian las dependencias, antes de ejecutar
-    //    el efecto de nuevo
-    return () => { // cleanup method
+    return () => { 
       console.log('cleanup')
       window.removeEventListener('pointermove', handleMove)
     }
   }, [enabled])
 
-  // [] -> solo se ejecuta una vez cuando se monta el componente
-  // [enabled] -> se ejecuta cuando cambia enabled y cuando se monta el componente
-  // undefined -> se ejecuta cada vez que se renderiza el componente
-
-  // change body className
   useEffect(() => {
     document.body.classList.toggle('no-cursor', enabled)
 
