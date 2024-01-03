3c4ecd7609c24d24436cd3879fa1be21663e85ce
JuanIgnacioMarchetto <tu@email.com>
2024-01-03 10:30:11 -0300

    3/1/2023-10:30

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,53 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 const FollowMouse = () => {
-  const [enabled, setEnabled] = useState(false)
-  const [position, setPosition] = useState({ x: 0, y: 0 })
-
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [imageURL, setImageURL] = useState('https://avatars.githubusercontent.com/u/43477348?v=4');
   useEffect(() => {
-    console.log('effect ', { enabled })
-
     const handleMove = (event) => {
-      const { clientX, clientY } = event
-      setPosition({ x: clientX, y: clientY })
-    }
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX - 455, y: clientY - 455  });
+    };
 
     if (enabled) {
-      window.addEventListener('pointermove', handleMove)
+      window.addEventListener('pointermove', handleMove);
     }
-
-    return () => { 
-      console.log('cleanup')
-      window.removeEventListener('pointermove', handleMove)
-    }
-  }, [enabled])
-
-  useEffect(() => {
-    document.body.classList.toggle('no-cursor', enabled)
 
     return () => {
-      document.body.classList.remove('no-cursor')
-    }
-  }, [enabled])
+      window.removeEventListener('pointermove', handleMove);
+    };
+  }, [enabled]);
+
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled);
+
+    return () => {
+      document.body.classList.remove('no-cursor');
+    };
+  }, [enabled]);
 
   return (
     <>
-      <div style={{
-        position: 'absolute',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        border: '1px solid #fff',
-        borderRadius: '50%',
-        opacity: 0.8,
-        pointerEvents: 'none',
-        left: -25,
-        top: -25,
-        width: 50,
-        height: 50,
-        transform: `translate(${position.x}px, ${position.y}px)`
-      }}
+      <img
+        src={imageURL}
+        alt="Imagen que sigue al mouse"
+        style={{
+          position: 'absolute',
+        
+          width: '50px', 
+          height: '50px', 
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }}
       />
       <button onClick={() => setEnabled(!enabled)}>
         {enabled ? 'Desactivar' : 'Activar'} seguir puntero
       </button>
     </>
-  )
-}
+  );
+};
 
-function App () {
-  return (
-    <main>
-      <FollowMouse />
-    </main>
-  )
-}
-
-export default App
+export default FollowMouse;
